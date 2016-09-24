c559ea62c30c8120633cf9042af77522e6995323
Neil McHenry <Neil McHenry>
2016-09-24 16:05:40 -0500

    First

---
+++ Assets/Scripts/DrawScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawScript : MonoBehaviour {
+
+ //   public SteamVR_TrackedObject trackedObj;
+ //   public GameObject trigger;
+
+ //   LineRenderer currLine;
+ //   int numClicks = 0;
+
+ //   public Color c1 = Color.white;
+ //   public Color c2 = new Color(1, 1, 1, 0);
+
+ //   // Update is called once per frame
+ //   void Update () {
+ //       SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+
+ //       if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+ //       {
+ //           GameObject go = new GameObject();
+ //           currLine = go.AddComponent<LineRenderer>();
+
+ //           currLine.SetWidth(0.5f, 0.5f);
+ //           currLine.SetColors(c1, c2);
+
+ //           numClicks = 0;
+ //       }
+ //       else if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+ //       {
+ //           currLine.SetVertexCount(numClicks + 1);
+ //           currLine.SetPosition(numClicks, new Vector3 (trigger.GetComponent<TriggerScript>().wall.position.x ,trigger.transform.position.y, trigger.transform.position.z));
+ //           numClicks++;
+ //       }
+	//}
+}
---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerScript : MonoBehaviour {
+    public SteamVR_TrackedObject trackedObj;
+    //bool erase = false;
+
+    //void Awake()
+    //{
+    //    trackedObj = GetComponent<SteamVR_TrackedObject>();
+    //}
+
+    //void Update()
+    //{
+    //    SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+
+    //    if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+    //    {
+    //        //Debug.Log("1");
+    //        erase = true;
+    //    }
+
+    //    if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+    //    {
+    //        //Debug.Log("2");
+    //        erase = false;
+    //    }
+    //}
+	void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Paint")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
